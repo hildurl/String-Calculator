42a25718b9c84ac44f2a03a5e5f5103b7375fcd2
hildurl <hildurl11@ru.is>
2014-10-17 22:41:55 +0000

    Lagfæring á lið 1 þannig að forritið kasti villu ef inntakið er strengur en ekki tölur

--- StringCalculator.java
+++ StringCalculator.java
@@ -3,22 +3,30 @@ public class StringCalculator{
 		System.out.println(Integer.toString(add("1,2")));
 		System.out.println(Integer.toString(add("")));
 		System.out.println(Integer.toString(add("1")));
+		System.out.println(Integer.toString(add("-1")));
+		System.out.println(Integer.toString(add("-2,-5")));
+		System.out.println(Integer.toString(add("aron,1")));	
 	}
 	private static int  add(String numbers){
-		if (numbers.length() == 0)
-		{
+		try{		
+			if (numbers.length() == 0)
+			{
+				return 0;
+			}
+			else if (!numbers.contains(","))
+			{
+				return Integer.parseInt(numbers);
+			}
+			else
+			{
+				int index = numbers.indexOf(","); 
+				int x = Integer.parseInt(numbers.substring(0,index));
+				int y = Integer.parseInt(numbers.substring(index+1,numbers.length())); 
+				return x + y;
+			}
+		} catch(NumberFormatException e) {
+			e.printStackTrace();
 			return 0;
 		}
-		else if (!numbers.contains(","))
-		{
-			return Integer.parseInt(numbers);
-		}
-		else
-		{
-			int index = numbers.indexOf(","); 
-			int x = Integer.parseInt(numbers.substring(0,index));
-			int y = Integer.parseInt(numbers.substring(index+1,numbers.length())); 
-			return x + y;
-		}		
 	}
 }
