273d5b657bd04cf97a23a43e95dac5da87052ac5
hildurl <hildurl11@ru.is>
2014-10-18 20:26:19 +0000

    3. Allow the Add method to handle new lines between numbers (instead of commas).

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,28 +1,39 @@
 public class StringCalculator{
 	public static void main(String []args) {
- 			
+ 		
 	}
-	private static int  add(String numbers){
+		private static int  add(String numbers){
 		try{		
 			if (numbers.length() == 0)
 			{
 				return 0;
 			}
-			else if (!numbers.contains(","))
+			else if (!numbers.contains(",") &&  !numbers.contains("\n"))
 			{
 				return Integer.parseInt(numbers);
 			}
 			else
 			{
 				int count = numbers.length() - numbers.replace(",","").length();
-				System.out.println(count + "");
+				count = count + numbers.length() - numbers.replace("\n","").length();
+					
 				int sum = 0;
 				for (int i = 0; i < count; i++)
 				{
-					int index = numbers.indexOf(","); 
-					int x = Integer.parseInt(numbers.substring(0,index));
-				 	sum = sum + x;
-					numbers = numbers.substring(index+1, numbers.length());
+					int index = numbers.indexOf(",");
+					int index2 = numbers.indexOf("\n");
+					if ((index < index2 && index != -1) || (index2 == -1)) 
+					{ 
+						int x = Integer.parseInt(numbers.substring(0,index));
+				 		sum = sum + x;
+						numbers = numbers.substring(index+1, numbers.length());
+					}
+					else if ((index2 < index && index2 != -1) || (index == -1))
+					{
+						int x = Integer.parseInt(numbers.substring(0,index2));
+                                                sum = sum + x;
+                                                numbers = numbers.substring(index2+1, numbers.length());	
+					}
 				}
 				sum = sum + Integer.parseInt(numbers);
 				
